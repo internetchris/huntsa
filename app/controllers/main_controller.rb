0212e775054e57c41fa92e97e7a32d533fe912a6
Chris Sund <chris@silhouettesolutions.net>
2011-03-19 12:24:10 -0600

    update logic

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,7 +5,7 @@ class MainController < ApplicationController
   end
   
   def trophy_room
-    @pictures = Picture.find(:all, :order => "created_at DESC", :conditions => ["front_page != ?", true])
+    @pictures = Picture.find(:all, :order => "created_at DESC")
   end
   
   def ranch
