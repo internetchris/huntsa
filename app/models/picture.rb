bd6d96ec4d1b3f3549bfbe4fe6ad36e67abb7a05
Chris Sund <chris@silhouettesolutions.net>
2011-03-19 12:43:05 -0600

    update pic size

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,5 +1,5 @@
 class Picture < ActiveRecord::Base
-   has_attached_file :picture, :styles => { :thumb => "250x250>", :large => "600x600" }
+   has_attached_file :picture, :styles => { :thumb => "250x250>", :medium => "350x350", :large => "600x600" }
    belongs_to :user
    
    validates_presence_of :shooter_name
